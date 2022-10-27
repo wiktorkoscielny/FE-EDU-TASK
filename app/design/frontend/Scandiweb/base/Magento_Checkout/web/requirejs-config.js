f7e05f1e8530bb263eac06722c0947508a04c783
Wiktor Koscielny <wiktor.koscielny@scandiweb.com>
2022-10-27 14:38:32 +0200

    Task done

---
+++ app/design/frontend/Scandiweb/base/Magento_Checkout/web/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'Magento_Checkout/js/sidebar': {
+                'Scandiweb_base/js/sidebar.js': true
+            }
+        }
+    }
+};
